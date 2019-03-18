7fb77949a715229c7f2cad0c9ca2c8b98828fda4
SurajShaw <48601484+SurajShaw@users.noreply.github.com>
2019-03-17 23:19:01 -1100

    Create FastIO.cpp
    Fast Input/Output code template for C++

---
+++ FastIO.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class fastio {
+public:
+    fastio() {
+        ios_base::sync_with_stdio(false);
+        cout.tie(nullptr);
+        cin.tie(nullptr);
+    }
+} __fastio;
+int main() {
+  __fastio;
+  return 0;
+}
