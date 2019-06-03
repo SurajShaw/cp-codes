919e802d77beb2ed3814be85fb0b4d3068275471
SurajShaw <48601484+SurajShaw@users.noreply.github.com>
2019-06-03 19:24:31 +0530

    Update CP_Template.cpp

--- CP_Template.cpp
+++ CP_Template.cpp
@@ -1,31 +1,10 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define watch(x) cout << (#x) << " is " << (x) << endl
 #define runtime() ((double)clock() / CLOCKS_PER_SEC)
 #define SZ(x) ((int)(x).size())
+#define FOR(i,s,e) for(int i=s; i<e; i++)
 #define WL(t) while(t --)
 #define LL long long int
 
